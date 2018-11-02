cfb686aee193ed8c4c53838a44081b05c57651b5
Jose Bentancour <josebentancour@gmail.com>
2018-11-02 12:51:05 -0300

    Delete compilar.sh

--- ejemplo_unicast/compilar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-make TARGET=zoul savetarget
-sudo make clean BOARD=remote-revb
-sudo make sender.upload BOARD=remote-revb WITH_TSCH=1 MOTES=/dev/ttyUSB0 NODEID=0x0014
-sudo make receiver.upload BOARD=remote-revb WITH_TSCH=1 MOTES=/dev/ttyUSB1 NODEID=0x0012
-sudo make login BOARD=remote-revb MOTES=/dev/ttyUSB1
