4444dffafaa7a0a262f3c3b1ef1b22d9ac024d3c
carocab <40127677+carocab@users.noreply.github.com>
2018-11-05 09:21:36 -0300

    Update compilar.sh

--- ejemplo_unicast/compilar.sh
+++ ejemplo_unicast/compilar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 make TARGET=zoul savetarget
 make clean BOARD=remote-revb
-make sender.upload BOARD=remote-revb WITH_TSCH=1 MOTES=/dev/ttyUSB0
-make receiver.upload BOARD=remote-revb WITH_TSCH=1 MOTES=/dev/ttyUSB1 NODEID=0x0013
+make sender.upload BOARD=remote-revb MOTES=/dev/ttyUSB0
+make clean BOARD=remote-revb
+make receiver.upload BOARD=remote-revb MOTES=/dev/ttyUSB1 NODEID=0x0013
 make login BOARD=remote-revb MOTES=/dev/ttyUSB1
